ac6439d42e1e78d261048acba7e2d21922f746bf
Fodieshub <buzkenny@gmail.com>
2021-01-04 20:13:31 +0100

    added class name to the li and text node

--- app.js
+++ app.js
@@ -22,6 +22,22 @@ function addTask(e) {
 
   // Create li elements
   const li = document.createElement('li');
+  // Add Class
+  li.className = 'collection-item';
+  // Create Textnode and append to li
+  li.appendChild(document.createTextNode(taskInput.value));
+  // Create New Link element
+  const link = document.createElement('a');
+  // Give clasd name to the link element
+  link.className = 'delete-item secondary-content';
+  // Add HTML icon
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+  // Append the link icon to the list item
+  li.appendChild(link);
+  // Append the list item to the UL
+  taskList.appendChild(li);
+
+  taskInput.value = '';
 
   e.preventDefault();
 }
